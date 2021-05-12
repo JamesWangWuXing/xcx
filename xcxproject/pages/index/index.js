4da42f9ba547611e1da5db30a273130f4e631768
DESKTOP-K2MAI9J\Admin <457790959@qq.com>
2021-05-12 14:09:29 +0800

    我的基本完成

--- xcxproject/pages/index/index.js
+++ xcxproject/pages/index/index.js
@@ -7,7 +7,11 @@ Page({
   onLoad: function () {
     console.log('Welcome to Mini Code')
   },
+  
   goToCourseDetail: function(){
     console.log("点击跳转到详情")
+    tt.navigateTo({
+      url: '/pages/course/course' // 指定页面的url
+    });
   }
 })
